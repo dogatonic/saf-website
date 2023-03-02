c23495bf047468b8e445c91d4d32d61420ba5cdf
Tucker <mail@matttucker.com>
2023-03-02 11:24:39 -0600

    Auto push failed. Why?

--- includes/page_parts/meetings_events.php
+++ includes/page_parts/meetings_events.php
@@ -36,7 +36,7 @@
 
 						<div class="col-12 col-md-4 mb-4">
 							<div class="basic_3_col pt-2" style="padding: 4px;">
-								<h4 class="text-dark text-uppercase">Birthday Night</h4>
+								<h4 class="text-dark text-uppercase">Birthday Night </h4>
 								<h5 style="color: red;">Last Friday of Each Month <br/> at 7:30 PM</h5>
 								<p><img src="/img/cake.png" alt="Birthday Night" style='width: 50%; height: auto;'></p>
 								<p>Northland Group celebrates annual lengths of sobriety on the last Friday of each month. To receive a chip, please sign up on the bulletine board in the Half Measures room no later than the Tuesday of Birthday week. FREE CAKE!</p>
