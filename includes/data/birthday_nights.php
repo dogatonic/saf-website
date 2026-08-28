607aef0875a6b820482dee7df22426ca8dbb2a82
Tucker <mail@matttucker.com>
2026-08-28 17:10:01 -0500

    I noticed a slight miscalc of Bday Night. Fixed code not to switch to the next months last Friday until after 10pm of current months last Friday

--- includes/data/birthday_nights.php
+++ includes/data/birthday_nights.php
@@ -1,21 +1,24 @@
 <?php
 
-// In PHP, write a function to Find the last Friday of the current month. If today is past that date, find the last Friday of the next month.
+// Find the last Friday of the current month. After 10 PM Chicago time
+// on that date, find the last Friday of the next month.
 
 function findLastFriday() {
-    $currentDate = new DateTime(); // Get the current date
+    $chicagoTimeZone = new DateTimeZone('America/Chicago');
+    $currentDate = new DateTime('now', $chicagoTimeZone);
     $currentMonth = $currentDate->format('Y-m');
-    $lastDayOfMonth = new DateTime("last day of $currentMonth");
+    $lastDayOfMonth = new DateTime("last day of $currentMonth", $chicagoTimeZone);
 
     // Find the last Friday of the current month
     while ($lastDayOfMonth->format('l') !== 'Friday') {
         $lastDayOfMonth->modify('-1 day');
     }
+    $lastDayOfMonth->setTime(22, 0);
 
-    // If today is past the last Friday of the current month, find the last Friday of next month
+    // After 10 PM on the last Friday, find the last Friday of next month.
     if ($currentDate > $lastDayOfMonth) {
         $nextMonth = $currentDate->modify('first day of next month')->format('Y-m');
-        $lastDayOfNextMonth = new DateTime("last day of $nextMonth");
+        $lastDayOfNextMonth = new DateTime("last day of $nextMonth", $chicagoTimeZone);
 
         while ($lastDayOfNextMonth->format('l') !== 'Friday') {
             $lastDayOfNextMonth->modify('-1 day');
